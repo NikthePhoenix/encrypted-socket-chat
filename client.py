0adc9b6a729d11154b0eb75cd22a263bc6e07da6
NikthePhoenix <nikgov23@gmail.com>
2024-10-03 23:59:45 +0530

    working basics

--- client.py
+++ client.py
@@ -1,17 +1,24 @@
-import socket
-def main():
-    HOST = 'your_public_ip' # Replace with your public IP address
-    PORT = 12345 # Replace with your desired port
+# Import socket module
+import socket               
+ 
+# Create a socket object
+s = socket.socket()         
+ 
+# Define the port on which you want to connect
+port = 12345               
+ 
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
-    
-    while True:
-        message = input('Enter message to send: ')
-        client_socket.sendall(message.encode('utf-8'))
-        if message.lower() == 'exit':
-            print('Disconnecting from server')
-            break
-    # To get server response
-        data = client_socket.recv(1024).decode('utf-8')
-        print(f'Server response: {data}')
+# Send data to server 'Hello world'
+
+## s.sendall('Hello World')
+
+input_string = input("Enter data you want to send->")
+s.sendall(bytes(input_string,'utf-8'))
+
+# receive data from the server
+print(s.recv(1024).decode())
+
+# close the connection
+s.close()
--- server.py
+++ server.py
@@ -1,32 +1,49 @@
-import socket
+# first of all import the socket library
+import socket               
+ 
+# next create a socket object
+s = socket.socket()         
+print("Socket successfully created")
+ 
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345               
+ 
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests 
+# coming from other computers on the network
+s.bind(('', port))        
+print("socket binded to",port)
+ 
+# put the socket into listening mode
+s.listen(5)     
+print("socket is listening")
+ 
+# a forever loop until we interrupt it or 
+# an error occurs
+while True:
+ 
+   # Establish connection with client.
+   c, addr = s.accept()     
+   print('Got connection from', addr)
 
-def receive_messages(sock):
-    while True:
-        try:
-            message = sock.recv(1024).decode('utf-8')
-            if message:
-                print(f'Client message: {message}')
-            else:
-                break
-        except Exception:
-            break
+   # Get data from client
+   data=c.recv(1024).decode()
+   
+   #Reverse data received from client
+   data = data[::-1]
 
-def main():
-    SERVER_IP = '127.0.0.1'
-    SERVER_PORT = 7560
-    
-    # Initialize and connect socket for the client
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_IP, SERVER_PORT))
-    
-    while True:
-        message = input('Enter message for client: ')
-        if message.lower() == 'exit':
-            print('Disconnecting from server')
-            break
-        else:
-            client_socket.sendall(message.encode('utf-8'))
-    client_socket.close()
-    
-    if __name__ == "__main__":
-        main()
+   if not data:
+      break
+   
+   # Send back reversed data to client   
+   c.sendall(bytes(data,'utf-8'))
+
+
+   # send a thank you message to the client. 
+   #c.send('\n Thank you for sending message!!!!')
+ 
+   # Close the connection with the client
+   c.close()
